501c26629c8d66842558fca6ddf28227ea4ca0b2
akdkun <3024954141@qq.com>
2026-04-28 20:42:11 +0800

    chore: 引入 Knife4j 并完成 OpenAPI 3 规范化配置，开启可视化 API 调试

--- src/main/java/com/csust/eco/config/Knife4jConfig.java
+++ src/main/java/com/csust/eco/config/Knife4jConfig.java
@@ -1,8 +1,11 @@
 package com.csust.eco.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +19,15 @@ public class Knife4jConfig {
                         .title("CSUST-EcoExchange 校园二手交易平台 API")
                         .version("V2.0")
                         .description("底层环境: JDK 21 + Spring Boot 3.2 + MyBatis-Plus")
-                        .contact(new Contact().name("csust-dev")));
+                        .contact(new Contact().name("csust-dev")))
+                // 1. 注册全局安全策略定义 (告诉 Swagger 我们用什么方式鉴权)
+                .components(new Components()
+                        .addSecuritySchemes("satoken", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY) // API Key 模式
+                                .in(SecurityScheme.In.HEADER)     // 参数放在 Header 中
+                                .name("satoken")                  // Sa-Token 默认的请求头名字
+                        ))
+                // 2. 将安全策略应用到所有的接口上
+                .addSecurityItem(new SecurityRequirement().addList("satoken"));
     }
 }
